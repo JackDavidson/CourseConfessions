ef56e1cb962e0d70cdc5ec5c10a4c1d45981bff8
Joseph Leovao <jleovao93@gmail.com>
2015-05-12 18:35:50 -0700

    Roger and Joseph: Implemented the PacementEditText class and PlacementImage class which will be used to make adding images and text to a class a lot easier

--- src/visuals/Placeable.java
+++ src/visuals/Placeable.java
@@ -1,9 +1,12 @@
 package visuals;
 
+import android.widget.EditText;
+
 public interface Placeable {
 	abstract void attachToScene();
 	abstract void setX(int x);
 	abstract void setY(int y);
 	abstract void setWidth(int width);
 	abstract void setHeight(int height);
+	abstract EditText getEditText();
 }
--- src/visuals/PlacementImage.java
+++ src/visuals/PlacementImage.java
@@ -1,6 +1,17 @@
 package visuals;
 
+import com.bitsplease.courseconfessions.R;
+
 import activities.BaseScene;
+//import activities.main.DTImageView;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class PlacementImage implements Placeable {
 
@@ -16,38 +27,73 @@ public class PlacementImage implements Placeable {
 	 * @param width
 	 * @param height
 	 */
-	public PlacementImage(BaseScene context, int x, int y, int width, int height) {
-
+	private DTImageView imageView;
+	private BaseScene context2;
+	private LinearLayout.LayoutParams vp;
+	private LinearLayout linearLayout;
+	private Bitmap bitmap;
+	public PlacementImage(BaseScene context, int x, int y, int width, int height) 
+	{
+		context2 = context;
+		linearLayout = new LinearLayout(context);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		bitmap = BitmapFactory.decodeResource(context.getResources(), R.raw.background);
+		imageView = new DTImageView(context);
+		vp = new LinearLayout.LayoutParams((int)(width*context.nativeToPxRatio), 
+			(int)(height*context.nativeToPxRatio));
+		//vp = new LinearLayout.LayoutParams(width, height);
+		imageView.setX(x* context.nativeToPxRatio);
+		imageView.imageBitmap = Bitmap.createScaledBitmap(bitmap,
+				(int)(width*context.nativeToPxRatio),(int)(height*context.nativeToPxRatio),true);
+		imageView.setY(y * context.nativeToPxRatio);
+		linearLayout.setClipChildren(false);
+		linearLayout.addView(imageView);
 	}
-
+	public EditText getEditText(){return null;}
 	@Override
 	public void attachToScene() {
 		// TODO Auto-generated method stub
-
+		context2.addContentView(linearLayout, vp);
+		
 	}
 
 	@Override
 	public void setX(int x) {
 		// TODO Auto-generated method stub
-
+		imageView.setX((context2.widthPx / 2) - x * context2.nativeToPxRatio);
 	}
 
 	@Override
 	public void setY(int y) {
 		// TODO Auto-generated method stub
-
+		imageView.setY((context2.heightPx * 1 / 2) + y * context2.nativeToPxRatio);
 	}
 
 	@Override
 	public void setWidth(int width) {
 		// TODO Auto-generated method stub
-
+		vp.width = (int) (width * context2.nativeToPxRatio);
 	}
 
 	@Override
 	public void setHeight(int height) {
 		// TODO Auto-generated method stub
-
+		vp.height = (int) (height * context2.nativeToPxRatio);
 	}
 
+	public class DTImageView extends View {
+
+	    public Bitmap imageBitmap;
+
+	    public DTImageView(Context context) { 
+	        super(context); 
+	    } 
+
+	    protected void onDraw(Canvas canvas) {
+	       super.onDraw(canvas);
+	       if(imageBitmap != null)
+	         canvas.drawBitmap(imageBitmap, 0, 0, null); 
+	    }
+	}
 }
+
