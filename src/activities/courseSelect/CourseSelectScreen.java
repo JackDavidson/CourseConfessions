3a76251f058fd5e9183f9c90b358f87340444df2
Eric Orozco <wookiee323@gmail.com>
2015-05-27 11:31:05 -0700

    Testing JSON

--- src/activities/courseSelect/CourseSelectScreen.java
+++ src/activities/courseSelect/CourseSelectScreen.java
@@ -1,5 +1,7 @@
 package activities.courseSelect;
 
+import java.util.ArrayList;
+
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.util.FPSLogger;
@@ -9,6 +11,7 @@ import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegion
 
 import user.User;
 import util.SDCardWriter;
+import util.phpInteractions;
 
 import com.bitsplease.courseconfessions.R;
 
@@ -17,6 +20,7 @@ import android.annotation.TargetApi;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.ViewGroup.LayoutParams;
@@ -34,88 +38,93 @@ import android.widget.Toast;
 /**
  * This is the main file, the entry point into the program. the REAL entry point
  * is onCreateScene and in what you define there.
- * 
+ *
  * Andengine provides a "SimpleBaseGameActivity from which your game activity
  * (in this case AndengineHello) should always be extended
- * 
+ *
  * implements IOnSceneTouchListener is for touch interface directly through the
  * screen, not through the buttons. if all interface goes through your buttons,
  * that shouldn't be necessary.
- * 
+ *
  * @author Jack - jack.davidson38@gmail.com
- * 
+ *
  */
 public class CourseSelectScreen extends BaseScene {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+   
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		/* ==== TODO set background ===== */
-		/* === END how to set background ==== */
+        /* ==== TODO set background ===== */
+        /* === END how to set background ==== */
 
-		/* ==== how to make the view scrollable === */
+        /* ==== how to make the view scrollable === */
 
-		// there are 3 objects here.
+        // there are 3 objects here.
+        phpInteractions course = new phpInteractions();
+       
+        ArrayList<String> courseList = course.getListOfCourses("CSE", 0, 0);
+        System.out.println(courseList.toString());
+       
+        ScrollView scroll = new ScrollView(this);
+        scroll.setBackgroundColor(Color.TRANSPARENT);
+        LayoutParams scrollLayoutParams = new LayoutParams(
+                LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 
-		ScrollView scroll = new ScrollView(this);
-		scroll.setBackgroundColor(Color.TRANSPARENT);
-		LayoutParams scrollLayoutParams = new LayoutParams(
-				LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+        TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT);
 
-		TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(
-				TableLayout.LayoutParams.WRAP_CONTENT,
-				TableLayout.LayoutParams.WRAP_CONTENT);
-		TableRow.LayoutParams rowParams = new TableRow.LayoutParams(
-				TableRow.LayoutParams.WRAP_CONTENT,
-				TableRow.LayoutParams.WRAP_CONTENT);
+        TableLayout tableLayout = new TableLayout(this);
+        tableLayout.setLayoutParams(tableParams);
 
-		TableLayout tableLayout = new TableLayout(this);
-		tableLayout.setLayoutParams(tableParams);
+        // first row
+        TableRow tableRow1 = new TableRow(this);// create a new row
+        tableRow1.setLayoutParams(tableParams); // set the params
+        TextView textView = new TextView(this);// add txt
+        textView.setText(courseList.toString());
+        textView.setLayoutParams(rowParams); // add txt
+        textView.setTextColor(Color.BLACK); // add txt
+        tableRow1.addView(textView); // add txt
+        CheckBox cb = new CheckBox(this); // add a check box
+        cb.setBackgroundColor(Color.BLACK);// add a check box
+        tableRow1.addView(cb); // add a check box
+        tableLayout.addView(tableRow1); // add txt
 
-		// first row
-		TableRow tableRow1 = new TableRow(this);// create a new row
-		tableRow1.setLayoutParams(tableParams); // set the params
-		TextView textView = new TextView(this);// add txt
-		textView.setText("this is a test \n\n\nn\n\n\n\n\n\n\n\n\n\\n\\\n\n\n\n\n\n\n\n\n\nhhhhh");
-		textView.setLayoutParams(rowParams); // add txt
-		textView.setTextColor(Color.BLACK); // add txt
-		tableRow1.addView(textView); // add txt
-		CheckBox cb = new CheckBox(this); // add a check box
-		cb.setBackgroundColor(Color.BLACK);// add a check box
-		tableRow1.addView(cb); // add a check box
-		tableLayout.addView(tableRow1); // add txt
+        // second row
+        TableRow tableRow2 = new TableRow(this);
+        tableRow2.setLayoutParams(tableParams);
+        TextView textView2 = new TextView(this);
+        textView2
+                .setText("this is another test \n\n\nn\n\n\n\n\n\n\n\n\n\\sadfdn\\\n\n\n\n\n\n\n\n\n\njjjjj");
+        textView2.setTextColor(Color.BLACK);
+        textView2.setLayoutParams(rowParams);
+        tableRow2.addView(textView2);
+        CheckBox cb2 = new CheckBox(this); // add a check box
+        cb2.setBackgroundColor(Color.BLACK);// add a check box
+        tableRow2.addView(cb2); // add a check box
+        tableLayout.addView(tableRow2);
 
-		// second row
-		TableRow tableRow2 = new TableRow(this);
-		tableRow2.setLayoutParams(tableParams);
-		TextView textView2 = new TextView(this);
-		textView2
-				.setText("this is another test \n\n\nn\n\n\n\n\n\n\n\n\n\\sadfdn\\\n\n\n\n\n\n\n\n\n\njjjjj");
-		textView2.setTextColor(Color.BLACK);
-		textView2.setLayoutParams(rowParams);
-		tableRow2.addView(textView2);
-		CheckBox cb2 = new CheckBox(this); // add a check box
-		cb2.setBackgroundColor(Color.BLACK);// add a check box
-		tableRow2.addView(cb2); // add a check box
-		tableLayout.addView(tableRow2);
+        scroll.addView(tableLayout);
+        addContentView(scroll, scrollLayoutParams);
+        /* == END how to make the view scrollable == */
 
-		scroll.addView(tableLayout);
-		addContentView(scroll, scrollLayoutParams);
-		/* == END how to make the view scrollable == */
+        // example code reading the ser file
+        String read = SDCardWriter.readFile(getFilesDir().toString()
+                + User.SAVE_FILE);
+        Toast.makeText(this, "Read file, result is:\n" + read,
+                Toast.LENGTH_LONG).show();
+        // end example code reading the user file
+        /* ====== reloading the User object ======== */
+        User user = new User(this);
+        String userNamePass = "username: " + user.getUserName() + "\n"
+                + "password: " + user.getPassword();
+        Toast.makeText(this, "Recreated user:\n" + userNamePass,
+                Toast.LENGTH_LONG).show();
+        /* ===== end reloading the user object ==== */
 
-		// example code reading the ser file
-		String read = SDCardWriter.readFile(getFilesDir().toString()
-				+ User.SAVE_FILE);
-		Toast.makeText(this, "Read file, result is:\n" + read,
-				Toast.LENGTH_LONG).show();
-		// end example code reading the user file
-		/* ====== reloading the User object ======== */
-		User user = new User(this);
-		String userNamePass = "username: " + user.getUserName() + "\n"
-				+ "password: " + user.getPassword();
-		Toast.makeText(this, "Recreated user:\n" + userNamePass,
-				Toast.LENGTH_LONG).show();
-		/* ===== end reloading the user object ==== */
-
-	}
+    }
 }
--- src/util/phpInteractions.java
+++ src/util/phpInteractions.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-
+import org.json.*;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -14,14 +14,15 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import user.User;
 import visuals.PlacementEditText;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
@@ -30,150 +31,182 @@ import android.widget.TextView;
 
 public class phpInteractions {
 
-	/* contact Eric/Jack/Nolen for questions on this file */
+    /* contact Eric/Jack/Nolen for questions on this file */
 
-	/**
-	 * TODO: comments, explaining method
-	 * 
-	 * @param username
-	 *            TODO
-	 * @param password
-	 *            TODO
-	 * @return TODO
-	 */
-	public static User attemptLoginAndCrateUser(String userName,
-			String userPass, Context context) throws LoginException {
-		/*
-		 * TODO: contact server, return success or failure. handle exceptions as
-		 * appropriate
-		 */
-		// if we're successful.
-		// TODO Auto-generated method stub
-		String stringResultUserName = null; // where the username will be put,
-		
-		Log.i("HomeScreen Attempt login username:", userName);
-		Log.i("HomeScreen Attempt login pass:", userPass);
+    /**
+     * TODO: comments, explaining method
+     *
+     * @param username
+     *            TODO
+     * @param password
+     *            TODO
+     * @return TODO
+     */
+    public static User attemptLoginAndCrateUser(String userName,
+            String userPass, Context context) throws LoginException {
+        /*
+         * TODO: contact server, return success or failure. handle exceptions as
+         * appropriate
+         */
+        // if we're successful.
+        // TODO Auto-generated method stub
+        String stringResultUserName = null; // where the username will be put,
+       
+        Log.i("HomeScreen Attempt login username:", userName);
+        Log.i("HomeScreen Attempt login pass:", userPass);
 
-		// the year data to send
-		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-		nameValuePairs.add(new BasicNameValuePair("username", userName));
-		nameValuePairs.add(new BasicNameValuePair("password", userPass));
+        // the year data to send
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("username", userName));
+        nameValuePairs.add(new BasicNameValuePair("password", userPass));
 
-		stringResultUserName = parseJSON(convertRespToString(httpPost(
-				nameValuePairs,
-				"http://www.courseconfessions.com/androidlogin.php")));
+        stringResultUserName = parseJSON(convertRespToString(httpPost(
+                nameValuePairs,
+                "http://www.courseconfessions.com/androidlogin.php")));
 
-		if(stringResultUserName == null){
-			throw new LoginException("Failed to connext to server");
-		} else if (stringResultUserName.equals("fail")) {
-			throw new LoginException("Incorrect Username or password");
-		}	else {
-			return new User(userName, userPass, stringResultUserName, context);
-		}
+        if(stringResultUserName == null){
+            throw new LoginException("Failed to connext to server");
+        } else if (stringResultUserName.equals("fail")) {
+            throw new LoginException("Incorrect Username or password");
+        }    else {
+            return new User(userName, userPass, stringResultUserName, context);
+        }
 
-	}
+    }
 
-	public static InputStream httpPost(ArrayList<NameValuePair> pairs,
-			String url) {
-		InputStream is = null;
-		
-		
+    public static InputStream httpPost(ArrayList<NameValuePair> pairs,
+            String url) {
+        InputStream is = null;
+       
+       
 
-		try {
-			//HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost(url);
-			httppost.setEntity(new UrlEncodedFormEntity(pairs));
-			//HttpResponse response = httpclient.execute(httppost);
-			
-			
-			HttpPoster httpPoster = (HttpPoster) new HttpPoster().execute(httppost);
-			
-			HttpResponse response = (HttpResponse) httpPoster.get(3, TimeUnit.SECONDS);
-			
-			HttpEntity entity = response.getEntity();
-			is = entity.getContent();
-		} catch (Exception e) {
-			Log.e("log_tag", "Error in http connection " + e.toString());
-		}
-		return is;
-	}
+        try {
+            //HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            httppost.setEntity(new UrlEncodedFormEntity(pairs));
+            //HttpResponse response = httpclient.execute(httppost);
+           
+           
+            HttpPoster httpPoster = (HttpPoster) new HttpPoster().execute(httppost);
+           
+            HttpResponse response = (HttpResponse) httpPoster.get(3, TimeUnit.SECONDS);
+           
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+        } catch (Exception e) {
+            Log.e("log_tag", "Error in http connection " + e.toString());
+        }
+        return is;
+    }
 
-	public static String convertRespToString(InputStream is) {
-		String result = "";
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					is, "iso-8859-1"), 8);
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			is.close();
+    public static String convertRespToString(InputStream is) {
+        String result = "";
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
 
-			result = sb.toString();
-			Log.e("result", result);
-		} catch (Exception e) {
-			Log.e("log_tag", "Error converting result " + e.toString());
-		}
-		return result;
-	}
+            result = sb.toString();
+            Log.e("result", result);
+        } catch (Exception e) {
+            Log.e("log_tag", "Error converting result " + e.toString());
+        }
+        return result;
+    }
 
-	public static String parseJSON(String result) {
-		String resultUserName = null; // where the username will be put,
-		try {
-			JSONArray jArray = new JSONArray(result);
-			Log.e("log_tag", "made it here");
-			for (int i = 0; i < jArray.length(); i++) {
-				JSONObject json_data = jArray.getJSONObject(i);
-				Log.i("", "made it to second");
-				Log.i("", json_data.getString("USER"));
-				resultUserName = json_data.getString("USER");
-				// Log.i("log_tag", "Length: " + json_data.length() + " "
-				// + "USER: " + json_data.getString("USER"));
-			}
-		} catch (JSONException e) {
-			Log.e("log_tag", "Error parsing data " + e.toString());
-		}
-		return resultUserName;
-	}
+    public static String parseJSON(String result) {
+        String resultUserName = null; // where the username will be put,
+        try {
+            JSONArray jArray = new JSONArray(result);
+            Log.e("log_tag", "made it here");
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject json_data = jArray.getJSONObject(i);
+                Log.i("", "made it to second");
+                Log.i("", json_data.getString("USER"));
+                resultUserName = json_data.getString("USER");
+                // Log.i("log_tag", "Length: " + json_data.length() + " "
+                // + "USER: " + json_data.getString("USER"));
+            }
+        } catch (JSONException e) {
+            Log.e("log_tag", "Error parsing data " + e.toString());
+        }
+        return resultUserName;
+    }
 
-	/**
-	 * TODO: comments
-	 * 
-	 * @param couseSection
-	 *            ex. CSE, BIO, etc.
-	 * @param lowNumber
-	 *            pass 0 for no minimum
-	 * @param highNumber
-	 *            pass 0 for no upper limit
-	 * @return TODO
-	 */
-	public static String[] getListOfCourses(String couseSection, int lowNumber,
-			int highNumber) {
-		/*
-		 * TODO: create a string array with all the couse sections and numbers
-		 * avaiable. ex. CSE 100, CSE 110. should only include results listed
-		 * under the courseSection. Note: courseSection is included to ensure
-		 * that this code can be expanded at a later date. for now, always pass
-		 * this function "CSE, 0, 0"
-		 */
-		return null;
-	}
-	
-	private static class HttpPoster extends AsyncTask<HttpPost, Void, HttpResponse> {
+    /**
+     * TODO: comments
+     *
+     * @param couseSection
+     *            ex. CSE, BIO, etc.
+     * @param lowNumber
+     *            pass 0 for no minimum
+     * @param highNumber
+     *            pass 0 for no upper limit
+     * @return TODO
+     */
+    public static ArrayList<String> getListOfCourses(String courseSection, int lowNumber,
+            int highNumber) {
+        /*
+         * TODO: create a string array with all the couse sections and numbers
+         * avaiable. ex. CSE 100, CSE 110. should only include results listed
+         * under the courseSection. Note: courseSection is included to ensure
+         * that this code can be expanded at a later date. for now, always pass
+         * this function "CSE, 0, 0"
+         */
+           
+        //course.add(new BasicNameValuePair("CourseSection", courseSection));
+        //nameValuePairs.add(new BasicNameValuePair("password", userPass));
+    	//String page = new Communicator().executeHttpGet("http://www.courseconfessions.com/androidgetcourselist.php");
+        HttpGet httpget = new HttpGet("http://www.courseconfessions.com/androidgetcourselist.php");
+        ArrayList<String> courseList = parseCourseJSON(httpget);
+
+        if(courseList.size() == 0){
+            try {
+                throw new LoginException("Failed to connext to server");
+            } catch (LoginException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return courseList;
+    }
+    @SuppressLint("NewApi")
+	public static ArrayList<String> parseCourseJSON(HttpGet g)
+    {
+        ArrayList<String> list = new ArrayList<String>(); // where the list of courses will be held
+        try {
+            JSONArray jArray = new JSONArray(g);
+            Log.e("log_tag", "made it here");
+            for (int i = 0; i < jArray.length(); i++) {
+                list.add(jArray.get(i).toString());
+                // Log.i("log_tag", "Length: " + json_data.length() + " "
+                // + "USER: " + json_data.getString("USER"));
+            }
+        } catch (JSONException e) {
+            Log.e("log_tag", "Error parsing data " + e.toString());
+        }
+        return list;
+    }
+   
+    private static class HttpPoster extends AsyncTask<HttpPost, Void, HttpResponse> {
 
         @Override
         protected HttpResponse doInBackground(HttpPost... params) {
-        	HttpClient httpclient = new DefaultHttpClient();
+            HttpClient httpclient = new DefaultHttpClient();
             try {
-				return httpclient.execute(params[0]);
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+                return httpclient.execute(params[0]);
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             return null;
         }
 
